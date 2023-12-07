2820e70c3008d8600db55d1b57ce830d23b78c56
zlyd00 <zlydo50@gmail.com>
2023-12-07 17:30:19 +0600

    .

---
+++ bot.py
@@ -0,0 +1,13 @@
+import asyncio
+import logging
+
+
+async def main():
+    return 1
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.error("Бот был выключен!")
