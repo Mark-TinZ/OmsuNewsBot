da72cd92e36d531170af1c2e600058f4fd979d6e
Mark <mixailkar55@gmail.com>
2024-01-14 02:08:11 +0600

    0.3.9 Co-authored-by: WuMT <wumt-dev@users.noreply.github.com>

--- omsu_bot/handlers/menu.py
+++ omsu_bot/handlers/menu.py
@@ -1,18 +1,72 @@
+from cgitb import text
 from mailbox import Message
+import aiogram
 import sqlalchemy as sa
 import sqlalchemy.orm as sorm
 
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup
+import aiogram.types
+from aiogram.utils.chat_action import ChatActionSender
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 import omsu_bot.data.language as lang
 from omsu_bot.fsm import HandlerState
 from omsu_bot.handlers import RouterHandler
+from omsu_bot.database.models import User, Student, Teacher
 
 
 class MenuForm(StatesGroup):
-	pass
+	async def main_menu_message(bot, context: FSMContext, user: aiogram.types.User = None):
+		data = await context.get_data()
+
+		if not bot.db.is_online():
+			return dict(
+				text=lang.user_error_database_connection
+			)
+
+		inline_menu = (
+			InlineKeyboardBuilder()
+				.button(text="Расписание", callback_data="main_menu_schedule")
+		)
+
+		sess: sorm.Session = bot.db.session
+
+		with sess.begin():
+			db_user: User | None = sess.execute(sa.select(User).where(User.tg_id == user.id)).scalar_one_or_none()
+			if db_user:
+
+				if db_user.role_id == "student":
+					student: Student = sess.execute(sa.select(Student).where(Student.user_id == db_user.id_)).scalar_one_or_none()
+					if student:
+						if student.is_moderator:
+							inline_menu.button(text="Админ-меню", callback_data="main_menu_admin")
+					else:
+						return dict(text=lang.user_error_database_logic)
+				
+				elif db_user.role_id == "teacher":
+					teacher: Teacher = sess.execute(sa.select(Teacher).where(Teacher.user_id == db_user.id)).scalar_one_or_none()
+					if not teacher:
+						return dict(text=lang.user_error_database_logic)
+			else:
+				return dict(
+					text=lang.user_error_auth_unknown
+				)
+		
+		
+		return dict(
+			text=lang.user_menu_description,
+			reply_markup=
+				inline_menu
+					.button(text="Настройкиsdlakaafskfsdkfsad", callback_data="main_menu_settings")
+					.button(text="О боте", callback_data="main_menu_about")
+					.adjust(1, 2)
+					.as_markup()
+		)
+	
+	
+	main_menu = HandlerState(message_handler=main_menu_message)
 
 
 class Menu(RouterHandler):
@@ -21,6 +75,7 @@ class Menu(RouterHandler):
 		
 		router: Router = self.router
 
-	@Router.message(F.text == "Расписание")
-	async def hendle_schedule(msg: Message):
-		pass
+		@router.message(F.text == "Главное меню")
+		async def handle_schedule(msg: Message, state: FSMContext):
+			async with ChatActionSender.typing(chat_id=msg.chat.id, bot=self.bot.tg):
+				MenuForm.main_menu.message_send(self.bot, state, msg.chat, msg.message_id)
--- omsu_bot/handlers/registration.py
+++ omsu_bot/handlers/registration.py
@@ -9,11 +9,11 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup
 from aiogram.types import Message, CallbackQuery, FSInputFile
 from aiogram.utils.chat_action import ChatActionSender
-from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder, InlineKeyboardButton, ReplyKeyboardBuilder
 
 import omsu_bot.data.language as lang
 from omsu_bot.fsm import HandlerState
-from omsu_bot.handlers import RouterHandler
+from omsu_bot.handlers import RouterHandler, menu
 from omsu_bot.database.models import Student, Teacher, User, Group
 
 
@@ -157,19 +157,24 @@ class Registration(RouterHandler):
 
 		@router.message(CommandStart())
 		async def handle_start(msg: Message, state: FSMContext) -> None:
+
+			if not self.bot.db.is_online():
+				await state.clear()
+				await msg.answer(text=lang.user_error_database_connection)
+				return
+
 			async with ChatActionSender.typing(chat_id=msg.chat.id, bot=self.bot.tg):
+
+				sender = msg.from_user
+
 				sess: sorm.Session = self.bot.db.session
 
 				if sess:
 					with sess.begin():
-						user: User = sess.execute(sa.select(User).where(User.tg_id == msg.from_user.id)).scalar_one_or_none()
-				else:
-					await state.clear()
-					await msg.answer(text=lang.user_error_database_connection)
-					return
+						user: User = sess.execute(sa.select(User).where(User.tg_id == sender.id)).scalar_one_or_none()
 
 				if user:
-					await msg.answer("Чем могу помочь?")
+					await menu.MenuForm.main_menu.message_send(self.bot, state, msg.chat, msg.message_id, user=sender)
 				else:
 					await RegistrationForm.greetings_approval.message_send(self.bot, state, msg.chat, msg.message_id)
 		
@@ -356,9 +361,14 @@ class Registration(RouterHandler):
 							success = True
 				
 				if success:
-					await call.message.edit_text(
+					await call.message.delete()
+					await call.message.answer(
 						text=f"👨‍🎓 *Студент*\n📚 *Курс №{course_number}*\n💼 *Группа: {group_name}*\n\nВы успешно зарегистрированы!",
-						parse_mode="Markdown"
+						parse_mode="Markdown",
+						reply_markup=
+							ReplyKeyboardBuilder()
+								.button(text="Главное меню")
+								.as_markup(resize_keyboard=True)
 					)
 				else:
 					await call.message.edit_text("При регистрации возникла ошибка...")
@@ -367,122 +377,6 @@ class Registration(RouterHandler):
 						await call.message.answer_video_note(video_note)
 		
 
-		# @router.message(CommandStart())
-		# async def handle_start(message: Message, state: FSMContext) -> None:
-		# 	get_state = await state.get_state()
-
-		# 	if get_state is not None:
-		# 		data = await state.get_data()
-		# 		msg = data.get("get_msg")
-		# 		if msg is not None:
-		# 			await message.bot.delete_message(message.chat.id, msg.message_id)
-		# 		await state.clear()
-
-		# 	sess: sorm.Session = self.bot.db.session
-		# 	with sess.begin():
-		# 		user: User = sess.execute(sa.select(User).where(User.tg_id == message.from_user.id)).scalar_one_or_none()
-
-		# 	if user is None:
-		# 		msg = await message.answer(greeting_message, reply_markup=super_inline_button)
-		# 		await state.update_data(get_msg=msg)
-		# 		await state.set_state(RegisterForm.get_super)
-		# 	else:
-		# 		await message.answer("Чем могу помочь?") #, reply_markup=menu_keyboard(message.from_user.id))
-		# 		pass
-
-		# @router.callback_query(RegisterForm.get_super, F.data == "super")
-		# async def on_super(call: CallbackQuery, state: FSMContext) -> None:
-		# 	await state.set_state(RegisterForm.get_agree)
-		# 	await call.message.edit_text(user_agreement_message, reply_markup=agree_inline_button)
-
-		# @router.callback_query(RegisterForm.get_agree, F.data == "agree")
-		# async def on_agree(call: CallbackQuery, state: FSMContext) -> None:
-		# 	await state.set_state(RegisterForm.get_role)
-		# 	await call.message.edit_text("Для начала вам нужно зарегистрироваться.\n\nВыберите роль:",
-		# 								reply_markup=choice_a_role_inline_keyboard)
-
-		# @router.callback_query(RegisterForm.get_role, F.data.in_({"student", "teacher"}))
-		# async def on_role_select(call: CallbackQuery, state: FSMContext) -> None:
-		# 	select_role = call.data
-
-		# 	if select_role == "student":
-		# 		await call.message.edit_text("Выберите курс:", reply_markup=choice_a_course_inline_keyboard)
-		# 		await state.set_state(RegisterForm.get_course)
-		# 	elif select_role == "teacher":
-		# 		await state.clear()  # TODO: Затычка
-		# 		await call.message.edit_text("В разработке...")
-		# 		await call.answer()
-
-		# @router.callback_query(RegisterForm.get_course, F.data.startswith("course_"))
-		# async def student_course_selected(call: CallbackQuery, state: FSMContext) -> None:
-			# course_number = int(call.data.split("_")[1])
-			# await state.update_data(course_number=course_number)
-
-			# sess: sorm.Session = self.bot.db.session
-			
-			# with sess.begin():
-			# 	res: sa.ScalarResult = sess.execute(sa.select(Group).where(Group.course_number == course_number)).scalars()
-			# groups = [(group.name, group.id_) for group in res]
-
-			# await call.message.edit_text("Выберите группу:", reply_markup=group_inline_keyboard(groups))
-			# await state.set_state(RegisterForm.get_group)
-
-
-		# @router.callback_query(RegisterForm.get_group, F.data.startswith("group_"))
-		# async def student_group_selected(call: CallbackQuery, state: FSMContext) -> None:
-		# 	try:
-		# 		spl = call.data.split("_")
-		# 		group_id = int(spl[1])
-		# 		group_name = spl[2]
-		# 	except Exception as e:
-		# 		await call.message.edit_text("При регистрации возникла ошибка...")
-		# 		async with ChatActionSender.upload_video_note(chat_id=call.message.chat.id, bot=self.bot.tg):
-		# 			video_note = FSInputFile("media/video/cat-huh.mp4")
-		# 			await call.message.answer_video_note(video_note)
-		# 		logging.error(e)
-		# 		return
-
-		# 	await state.update_data(group_id=group_id)
-		# 	data = await state.get_data()
-		# 	course = data["course_number"]
-		# 	await call.message.edit_text(f"Курс: {course}\nГруппа: {group_name}\n\nВсе верно?",
-		# 								reply_markup=yes_or_back_inline_keyboard)
-
-		# @router.callback_query(F.data == "yes")
-		# async def student_data_confirmed(call: CallbackQuery, state: FSMContext) -> None:
-		# 	# await call.message.delete()
-
-			# data = await state.get_data()
-			# group_id = data.get("group_id", None)
-
-			# await state.clear()
-
-			# success = False
-
-			# tg_id = call.from_user.id
-			# sess: sorm.Session = self.bot.db.session
-			# with sess.begin():
-			# 	pk = sess.execute(sa.insert(User).values(tg_id=tg_id, role_id="student")).inserted_primary_key
-			# 	if pk:
-			# 		student = Student(user_id=pk[0], group_id=group_id)
-			# 		sess.add(student)
-			# 		success = True
-
-		# 	if success:
-		# 		await call.message.edit_text("Отлично! Вы успешно зарегистрированы. Приятного пользования!")
-		# 	else:
-		# 		await call.message.edit_text("При регистрации возникла ошибка...") #, reply_markup=menu_keyboard(call.from_user.id))
-		# 		async with ChatActionSender.upload_video_note(chat_id=call.message.chat.id, bot=self.bot.tg):
-		# 			video_note = FSInputFile("media/video/cat-huh.mp4")
-		# 			await call.message.answer_video_note(video_note)
-
-		# @router.callback_query(F.data.startswith('back_'))
-		# async def process_back(call: CallbackQuery, state: FSMContext) -> None:
-		# 	data = call.data
-			
-		# 	match data:
-		# 		case "back_course":
-		# 			pass
 
 
 	
