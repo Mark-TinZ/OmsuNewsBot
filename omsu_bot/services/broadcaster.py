b916e4f204552cec0e03a09ef4fd130426007be9
Mark <mixailkar55@gmail.com>
2024-02-28 13:02:08 +0600

    0.5.4

--- omsu_bot/services/broadcaster.py
+++ omsu_bot/services/broadcaster.py
@@ -15,7 +15,7 @@ class Broadcast():
 			protect_content: bool = False, 
 			disable_notification: bool = False, 
 			flood_sleep: bool = False
-	):
+	) -> None:
 		self.bot: Bot = bot
 		self.chat_ids: list[Union[int, str]] = chat_ids
 		self.protect_content: bool = protect_content
@@ -23,7 +23,7 @@ class Broadcast():
 		self.flood_sleep: bool = flood_sleep
 
 
-	async def send_message(self, *args, **kwargs) -> bool:
+	async def send_message(self, *args, **kwargs) -> None:
 		for chat_id in self.chat_ids:
 			try:
 				await self.bot.send_message(
@@ -44,12 +44,12 @@ class Broadcast():
 			except exceptions.TelegramForbiddenError:
 				logger.error(f"Forbidden seding message to the user ({chat_id})")			
 
-	async def send_photo(self, *args, **kwargs) -> bool:
+	async def send_photo(self, *args, **kwargs) -> None:
 		for chat_id in self.chat_ids:
 			try:
 				await self.bot.send_photo(
 					*args,
-					user_id=chat_id,
+					chat_id=chat_id,
 					disable_notification=self.disable_notification,
 					**kwargs
 				)
@@ -58,7 +58,7 @@ class Broadcast():
 				await asyncio.sleep(e.retry_after)
 				await self.bot.send_photo(
 					*args,
-					user_id=chat_id,
+					chat_id=chat_id,
 					disable_notification=self.disable_notification,
 					**kwargs
 				)
